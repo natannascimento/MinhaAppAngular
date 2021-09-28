704d2776391b392c4807191dae43a6c4369e904b
natannascimento <natan.graves@gmail.com>
2021-09-28 01:53:48 -0300

    Testando Decorators e Modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NomeComponenteComponent } from './pasta/nome-componente/nome-componente.component';
+import { HomeComponent } from './home/home.component';
+import { ServicoService } from './servico.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NomeComponenteComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    ServicoService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/funcionalidade/funcionalidade.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [
+    //seus componentes
+  ],
+  imports: [
+    CommonModule
+    //seus modulos
+  ],
+  providers: [
+    //seus serviços
+  ]
+})
+export class FuncionalidadeModule { }
